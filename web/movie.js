70e4792b8142e4ee40abdfdcbf44ed7a60727bc3
Ian <typppo@gmail.com>
2013-02-02 19:42:38 -0800

    open in new window

--- web/movie.js
+++ web/movie.js
@@ -81,7 +81,7 @@
           new THREE.Vector3(-389.42, -891.5953, -1420.21459),  // position
           new THREE.Vector3(2.5839369989874, -0.206877525252764, 3.0141779097761026),     // rotation
           50,   // fov
-          "This is part of the <a href=\"http://www.mpa-garching.mpg.de/galform/virgo/millennium/\">Millenium Run</a>, the largest n-body supercomputer simulation ever."
+          "This is part of the <a href=\"http://www.mpa-garching.mpg.de/galform/virgo/millennium/\" target=\"_blank\">Millenium Run</a>, the largest n-body supercomputer simulation ever."
           + "<br><br>In a cube of space 2 billion light years wide, it traced the evolution of over 10 billion pieces of dark matter, each a billion times the mass of our sun, roughly equivalent to 20 million galaxies."
           + "<br><br>You're looking at a billion cubic light-years of space -- unthinkably huge, but just a tiny part of the universe."
           + "<br><br>In this view, our entire galaxy is smaller than a single point."
